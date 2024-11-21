42b4bc3fae8c56a90c4d2a8967097f69e2eab48f
Meiriane Ferreira <meirianesouza02@gmail.com>
2024-11-21 11:14:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const pool = require("./conexaobanco");
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 // Rota para renderizar o formulário de cadastro
 app.get('/cadastro', (req, res) => {
@@ -18,9 +20,7 @@ app.get('/cadastro', (req, res) => {
 // Rota para salvar novos dados de cliente
 app.post('/cadastro', function(req, res) {
     const { nome, sobrenome, email, whatsapp, cep, logradouro, numero, complemento, bairro, cidade, estado } = req.body;
-
-    const sql = 'INSERT INTO dadoscliente (nome, sobrenome, email, whatsapp, cep, logradouro, numero, complemento, bairro, estado, cidade) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-
+    const sql = 'INSERT INTO dadoscliente (nome, sobrenome, email, whatsapp, cep, logradouro, numero, complemento, bairro, cidade, estado) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
     pool.query(sql, [nome, sobrenome, email, whatsapp, cep, logradouro, numero, complemento, bairro, cidade, estado], function(error, result) {
         if (error) throw error;
         res.redirect("listadeclientes");
@@ -36,35 +36,13 @@ app.get('/listadeclientes', function(req, res) {
     });
 });
 
-// Rota para deletar um cliente
-app.get('/delete-dadoscliente', function(req, res) {
-    var codcliente = req.query.codcliente;
-
-    if (!codcliente) {
-        return res.status(400).send('ID do cliente não fornecido. Por favor, forneça um ID válido.');
-    }
-
-    console.log('ID do cliente para deletar:', codcliente);
-
-    var sql = "DELETE FROM dadoscliente WHERE codcliente=?";
-    pool.query(sql, [codcliente], function(error, result) {
-        if (error) {
-            console.log(error);
-            return res.status(500).send('Erro ao deletar cliente');
-        }
-        res.redirect('/listadeclientes');
-    });
-});
-
 // Rota para exibir o formulário de atualização de dados do cliente
 app.get('/update-dadoscliente', function(req, res) {
     var codcliente = req.query.codcliente;
     console.log('codcliente recebido para update:', codcliente);
-
     var sql = "SELECT * FROM dadoscliente WHERE codcliente=?";
     pool.query(sql, [codcliente], function(error, result) {
         if (error) console.log(error);
-
         if (result && result.length > 0) {
             res.render("alterarcadastro", { dadoscliente: result[0] });
         } else {
@@ -75,23 +53,9 @@ app.get('/update-dadoscliente', function(req, res) {
 
 // Rota POST para atualizar os dados do cliente no banco de dados
 app.post('/update-dadoscliente', function(req, res) {
-    const nome = req.body.nome;
-    const sobrenome = req.body.sobrenome;
-    const email = req.body.email;
-    const whatsapp = req.body.whatsapp;
-    const cep = req.body.cep;
-    const logradouro = req.body.logradouro;
-    const numero = req.body.numero;
-    const complemento = req.body.complemento;
-    const bairro = req.body.bairro;
-    const cidade = req.body.cidade;
-    const estado = req.body.estado;
-    const codcliente = req.body.codcliente;
-    
+    const { nome, sobrenome, email, whatsapp, cep, logradouro, numero, complemento, bairro, cidade, estado, codcliente } = req.body;
     console.log('codcliente recebido para atualização:', codcliente);
-
-    const sql = "UPDATE dadoscliente SET nome=?, sobrenome=?, email=?, whatsapp=?, cep=?, logradouro=?, numero=?, complemento=?, bairro=?, estado=?, cidade=? WHERE codcliente=?";
-
+    const sql = "UPDATE dadoscliente SET nome=?, sobrenome=?, email=?, whatsapp=?, cep=?, logradouro=?, numero=?, complemento=?, bairro=?, cidade=?, estado=? WHERE codcliente=?";
     pool.query(sql, [nome, sobrenome, email, whatsapp, cep, logradouro, numero, complemento, bairro, cidade, estado, codcliente], function(error, result) {
         if (error) {
             console.log(error);
@@ -101,6 +65,28 @@ app.post('/update-dadoscliente', function(req, res) {
     });
 });
 
-app.listen(5000, () => {
-    console.log('Servidor rodando na porta 5000');
+
+/* Rota para deletar um cliente*/
+app.post('/delete-dadoscliente', function(req, res) {
+    var codcliente = req.body.codcliente;
+ 
+    if (!codcliente) {
+        return res.status(400).send('ID do cliente não fornecido. Por favor, forneça um ID válido.');
+    }
+ 
+    console.log('ID do cliente para deletar:', codcliente); // Adicione um log para confirmar o valor de codcliente
+ 
+    var sql = "DELETE FROM dadoscliente WHERE codcliente=?";
+    pool.query(sql, [codcliente], function(error, result) {
+        if (error) {
+            console.log(error); // Log de erro detalhado
+            return res.status(500).send('Erro ao deletar cliente');
+        }
+        console.log('Cliente deletado com sucesso:', result); // Log de sucesso
+        res.redirect('/listadeclientes');
+    });
+});
+
+app.listen(5001, () => {
+    console.log('Servidor rodando na porta 5001');
 });
